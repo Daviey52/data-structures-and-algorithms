ddae90d4e2672135bf50eab4c44959533657f126
Daviey52 <davidkariska@gmail.com>
2022-01-23 09:59:42 -0800

    brackets

---
+++ python/code_challange/bracket/README.md
@@ -0,0 +1,22 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+Write a function called validate brackets
+Arguments: string
+Return: boolean
+representing whether or not the brackets in the string are balanced
+There are 3 types of brackets:
+
+Round Brackets : ()
+Square Brackets : []
+Curly Brackets : {}
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big 0 time :(1)
+      space : (n)
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
---
+++ python/code_challange/test/test_brackets.py
@@ -0,0 +1,43 @@
+from bracket.brackets import Brackets
+import pytest
+
+def test_bracket_1():
+    str1 = "[]"
+    assert Brackets().validating_brackets(str1) == True
+
+
+def test_bracket_1():
+    str1 = "[]"
+    assert Brackets().validating_brackets(str1) == True
+
+def test_bracket_2():
+    str1 = "{}(){}"
+    assert Brackets().validating_brackets(str1) == True
+
+@pytest.mark.skip("pending")
+def test_bracket_3():
+    str1 = "()[[Extra characters]]"
+    assert Brackets().validating_brackets(str1) == True
+
+@pytest.mark.skip("pending")
+def test_bracket_4():
+    str1 = "{}{Code}[Fellows](())"
+    assert Brackets().validating_brackets(str1) == True
+
+def test_bracket_5():
+    str1 = "[({}]"
+    assert Brackets().validating_brackets(str1) == False
+
+def test_bracket_6():
+    str1 = "(]("
+    assert Brackets().validating_brackets(str1) == False
+
+def test_bracket_7():
+    str1 = "["
+    assert Brackets().validating_brackets(str1) == False
+
+def test_bracket_8():
+    str1 = "{(}}"
+    assert Brackets().validating_brackets(str1) == False
+
+
