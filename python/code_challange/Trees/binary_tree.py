e86d5f923274d3920953d4039125509235443e1e
Daviey52 <davidkariska@gmail.com>
2022-01-30 21:33:19 -0800

    updates

--- python/code_challange/Trees/binary_tree.py
+++ python/code_challange/Trees/binary_tree.py
@@ -1,27 +1,43 @@
 class BinaryTree:
     def __init__(self,root=None):
         self.root = root
+        self.left = None
+        self.right = None
 
     def pre_order(self):
         ## root > left > right
         values = []
-
         def traverse(root):
             if root is None:
                 return
-
             values.append(root.value)
             traverse(root.left)
             traverse(root.right)
         traverse(self.root)
         return values
-    def in_order():
+    def in_order(self):
         ## left > root > right
-        pass
+        values = []
+        def traverse(root):
+            if root is None:
+                return
+            traverse(root.left)
+            values.append(root.value)
+            traverse(root.right)
+        traverse(self.root)
+        return values
 
-    def post_order():
+    def post_order(self):
         # left > right > root
-        pass
+        values = []
+        def traverse(root):
+            if root is None:
+                return
+            traverse(root.left)
+            traverse(root.right)
+            values.append(root.value)
+        traverse(self.root)
+        return values
 
 
 
