55dbffb89b571702d181611f7787b0d2803d28ad
Daviey52 <davidkariska@gmail.com>
2022-02-03 05:40:07 -0800

    breadth-first

---
+++ python/code_challange/breath_first_traversal/breadth_first_traversal.py
@@ -0,0 +1,11 @@
+import queue
+from Trees.binary_tree import BinaryTree
+from stack.queue import Queue
+from Trees.node import Node
+
+def breadth_first(tree):
+
+    queue = Queue
+    visited = []
+
+    pass
