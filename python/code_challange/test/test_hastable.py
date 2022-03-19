31cec5e4dcf1d9227a3c1431d08942e4a9dfa8dc
Daviey52 <davidkariska@gmail.com>
2022-03-18 23:26:02 -0700

    hashtable

--- python/code_challange/test/test_hastable.py
+++ python/code_challange/test/test_hastable.py
@@ -1,1 +1,32 @@
 from hashtable.hashtable import Hashtable
+import pytest
+
+
+def test_hash():
+    hashtable=Hashtable()
+    actual = hashtable.hash('gold')
+    expected = 998
+    assert actual == expected
+
+def test_hash_empty():
+    hashtable=Hashtable()
+    actual = hashtable.hash('')
+    expected = 0
+    assert actual == expected
+
+def test_hash_contains():
+    hashtable=Hashtable()
+    hashtable.add('gold', 'dav')
+    actual = hashtable.contains('gold')
+    expected = True
+    assert actual == expected
+
+def test_hash_get():
+    hashtable=Hashtable()
+    hashtable.add('gold','dav')
+    actual = hashtable.get('gold')
+    expected = 'dav'
+    assert actual == expected
+
+
+
